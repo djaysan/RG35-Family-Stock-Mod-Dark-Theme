f2705209c71b39d9ce1b961b2a7e268d631c7d10
DJaySAN <djaysan@hotmail.com>
2025-01-28 23:10:59 +0100

    Added initial files

---
+++ APPS/Theme - Absolute Black.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+####################################################################
+#                        New Theme Installer                       #
+#                        made by @infra-blue                       #
+####################################################################
+
+SH_DIR="$(cd $(dirname "$0"); pwd)"
+THEME_NAME=$(basename "${0%.*}")
+TARGET_DIR="/mnt/vendor"
+BOOT_MOUNT="/mnt/boot"
+BOOT_PARTITION="/dev/mmcblk0p2"
+
+if [ -d "${TARGET_DIR}/res1" ] && [ -d "${TARGET_DIR}/res2" ]; then
+    cp -rf "${SH_DIR}/${THEME_NAME}"/res1 "${TARGET_DIR}"/
+    cp -rf "${SH_DIR}/${THEME_NAME}"/res2 "${TARGET_DIR}"/
+fi
+sync
+
+if [ ! -d "${BOOT_MOUNT}" ]; then
+  mkdir "${BOOT_MOUNT}"
+  mount "${BOOT_PARTITION}" "${BOOT_MOUNT}"
+
+  cp -rf "${SH_DIR}/${THEME_NAME}"/boot/* "${BOOT_MOUNT}"/
+
+  sync
+
+  umount "${BOOT_MOUNT}"
+  rmdir "${BOOT_MOUNT}"
+fi
+exit 0
